317df5297ed28569a988329b1276d5cbc1e187a4
VaibhavGupta21525 <85351135+VaibhavGupta21525@users.noreply.github.com>
2022-08-26 23:49:42 +0530

    Added FormActivity

---
+++ app/src/main/java/in/udip/astitva/ui/form/FormAdapter.kt
@@ -0,0 +1,26 @@
+package `in`.udip.astitva.ui.form
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class FormAdapter (private val context: Context):
+    RecyclerView.Adapter<FormAdapter.FormViewHolder>() {
+
+    class FormViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FormViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: FormViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
