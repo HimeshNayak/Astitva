05960a114a67b12f2db4715115ea81762de2b865
HimeshNayak <himeshnayak015@gmail.com>
2022-08-26 13:19:49 +0530

    account fragment

--- app/src/main/java/in/udip/astitva/ui/account/AccountFragment.kt
+++ app/src/main/java/in/udip/astitva/ui/account/AccountFragment.kt
@@ -1,5 +1,6 @@
-package `in`.udip.astitva.ui.notifications
+package `in`.udip.astitva.ui.account
 
+import `in`.udip.astitva.databinding.FragmentAccountBinding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,11 +8,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import `in`.udip.astitva.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class AccountFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentAccountBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val accountViewModel =
+            ViewModelProvider(this).get(AccountViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textAccount
+        accountViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
