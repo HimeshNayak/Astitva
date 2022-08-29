eadea22f1204224d3ba74c68f1530528fa2bb316
HimeshNayak <himeshnayak015@gmail.com>
2022-08-29 11:50:51 +0530

    undefined variable bug fixing

--- app/src/main/java/in/udip/astitva/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/in/udip/astitva/ui/dashboard/DashboardFragment.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+//import androidx.lifecycle.ViewModelProvider
 import `in`.udip.astitva.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -24,18 +23,12 @@ class DashboardFragment : Fragment() {
     ): View {
 
 
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+//        val dashboardViewModel =
+//            ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
 
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
 
     }
 
